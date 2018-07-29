f526f8b119b19f696a522d8f0f8f56cdc4c82cc1
Steve Rayner <srayner02@gmail.com>
2018-07-29 17:57:04 +0100

    Added CORS handling

--- app.js
+++ app.js
@@ -10,6 +10,17 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+// CORS handling
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT', 'POST', 'PATCH', 'DELETE', 'GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 // Routes.
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
